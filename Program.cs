178c3a815d0a005c2fddfb7f7fe91cff1e28be42
LAPTOP-696GJNV6\hemer <hemersondamasceno90@gmail.com>
2023-01-29 12:43:24 -0300

    Desafio realizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
+using static System.Console;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+WriteLine("Smartphone Nokia:");
+var nokia = new Nokia("999999999", "Galaxy S22 Ultra", "123456789", 256);
+nokia.Ligar();
+nokia.InstalarAplicativo("Dio");
+
+WriteLine();
+
+WriteLine("Smartphone Iphone:");
+var Iphone = new Iphone("888888888", "Galaxy S20 FE ", "987654321", 128);
+Iphone.ReceberLigacao();
+Iphone.InstalarAplicativo("Dio");
